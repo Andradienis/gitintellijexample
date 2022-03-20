5e90d69e4ba50a318e35e9e468e16f48320cd83c
Andrius <vasiliauskas.andrius@yahoo.com>
2022-03-20 17:31:23 +0200

    add printUserA() into UserA.java
    labas vakaras
    labas rytas
    labas naktis

--- src/main/java/lt/vcs/gitintellijexample/Main.java
+++ src/main/java/lt/vcs/gitintellijexample/Main.java
@@ -1,10 +1,11 @@
 package lt.vcs.gitintellijexample;
 
 public class Main {
+
     public static  void main(String[] args){
 
-
-
+        UserA userA = new UserA();
+        userA.printUserA();
     }
 
 }
